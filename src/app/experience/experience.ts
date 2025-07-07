015ea532bf27bf2fd54d1753fa790c357150ec85
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-07-07 11:02:26 +0200

    class custom finis. partie CV bien avancé

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './experience.css'
 })
 export class Experience {
+  calculateAge(birthDateString: string): number {
+    const today = new Date();
+    const birthDate = new Date(birthDateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
 
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  }
+
+  age = this.calculateAge('1998-02-12');
 }
