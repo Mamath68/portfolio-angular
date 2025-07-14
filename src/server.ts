28a7d8e9c56aba1a3cae771f16ad832880b04b9e
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-07-14 14:05:40 +0200

    modifications des logos de compétences. css reste à revoir, mais dans l'ensemble, ça va

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
-import { join } from 'node:path';
+import {join} from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 
 /**
  * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ * The server listens to the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
