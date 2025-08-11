6245971e4ec2b1975b04c04544dccf0ac68b4767
Mamath68 <mathieu.stamm@gmail.com>
2025-08-11 16:47:54 +0200

    modification des icones des skill et ajout d'une information sur la page d'accueil. a modifier.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+    imports: [
+        NgOptimizedImage
+    ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  calculateAge(birthDateString: string): number {
+    const today = new Date();
+    const birthDate = new Date(birthDateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  }
+  mathieu = "assets/img/mathieu.jpg";
 
 }
